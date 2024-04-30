b3b7ef0c7b3f0d702cfee69454c655e08bc5cbe7
Nguyen Duc Chung <102210030@sv1.dut.udn.vn>
2024-04-30 17:17:40 +0700

    Merge remote-tracking branch 'origin/chung' into chung
    # Conflicts:
    #	Backend/.idea/vcs.xml
    #	Backend/Router/__init__.py

--- Backend/Router/auth_router.py
+++ Backend/Router/auth_router.py
@@ -12,21 +12,39 @@ auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
 
 @auth_router.post("/token")
-async def get_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Res_Token:
-    teacher_service = BO_teacher()
-    teacher: Teacher = teacher_service.authenticate(form_data.username, form_data.password)
-    token = None
-    if teacher:
-        token = create_access_token({"id": teacher.id, "email": teacher.email, "role": 'teacher'})
+async def get_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], role: str) -> Res_Token:
+    if role == "teacher":
+        teacher_service = BO_teacher()
+        teacher: Teacher = teacher_service.authenticate(form_data.username, form_data.password)
+        if not teacher:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        token = create_access_token({"id": teacher.id, "email": teacher.email, "role": role})
+
         return Res_Token(**{"access_token": token, "token_type": 'bearer'})
 
-    student_service = BO_student()
-    student: Student = student_service.authenticate(form_data.username, form_data.password)
-    if student:
-        token = create_access_token({"id": student.id, "email": student.email, "role": 'student'})
+    if role == "student":
+        student_service = BO_student()
+        student: Student = student_service.authenticate(form_data.username, form_data.password)
+        if not student:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        token = create_access_token({"id": student.id, "email": student.email, "role": role})
+
         return Res_Token(**{"access_token": token, "token_type": 'bearer'})
 
-    if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    if role == "admin":
+        pass
 
-
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Need role: ['teacher', 'student', 'admin']",
+    )
