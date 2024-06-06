bd1167b59be1cb0a72de6f2402f702ad97572d25
Nguyen Duc Chung <102210030@sv1.dut.udn.vn>
2024-06-06 14:48:40 +0700

    Merge remote-tracking branch 'origin/chung' into chung
    # Conflicts:
    #	Backend/.idea/vcs.xml
    #	Backend/Router/__init__.py

--- Backend/main.py
+++ Backend/main.py
@@ -5,14 +5,16 @@ import logging
 logging.getLogger('passlib').setLevel(logging.ERROR)
 
 # (fastAPI) PBL5---Quizit> uvicorn Backend.main:app --host 0.0.0.0 --port 4444
-# (fastAPI) PBL5---Quizit> daphne Backend.main:app --bind 0.0.0.0 --port 4444
 
 
 # MULTIPLE WORKERS
 # FOR WSL LINUX
 # (fastAPI) PBL5---Quizit> gunicorn Backend.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:4444
 
-# sudo kill -9 $(netstat -ltnp | grep ':4444' | awk '{print $7}' | cut -d'/' -f1)
+# Find the PID of the process running on port 4444
+# pid=$(lsof -t -i:4444)
+# Kill the process
+# kill -9 $pid
 
 # Accessing a WSL 2 distribution from your local area network (LAN)
 # netsh interface portproxy add v4tov4 listenport=<yourPortToForward> listenaddress=0.0.0.0 connectport=<yourPortToConnectToInWSL> connectaddress=(wsl hostname -I)
