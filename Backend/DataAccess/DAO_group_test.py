3e5cd99d310c0c27336a30d8e94b3e268e12cb93
Nguyen Duc Chung <102210030@sv1.dut.udn.vn>
2024-06-09 23:12:15 +0700

    Merge remote-tracking branch 'origin/chung' into chung
    # Conflicts:
    #	Backend/.idea/vcs.xml
    #	Backend/Router/__init__.py

--- Backend/DataAccess/DAO_group_test.py
+++ Backend/DataAccess/DAO_group_test.py
@@ -45,7 +45,7 @@ class DAO_group_test:
             while True:
                 try:
                     cursor.execute(
-                        "INSERT INTO [group_test]([id], [group_id], [test_id], [name], [start], [end], [duration], [shuffle], [hash_pswd], [tolerance], [n_page], [allow_move])  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                        "INSERT INTO [group_test]([id], [group_id], [test_id], [name], [start], [end], [duration], [shuffle], [hash_pswd], [tolerance], [n_page], [allow_move])  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
                         (id, data.group_id, data.test_id, data.name, data.start, data.end, data.duration, data.shuffle, data.hash_pswd, data.tolerance, data.n_page, data.allow_move))
                     return id
                 except pymssql.Error as e:
